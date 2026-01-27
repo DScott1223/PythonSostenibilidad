c4431945dcc3842ac4f1f52dcff82d752c89da4c
D Scott <diegoscottm@gmail.com>
2026-01-27 09:17:56 +0100

    ejercicios tplas y diccionarios

--- ejerciciostema3/estructuras.datos/EjerciciosEstructurasDatos.py
+++ ejerciciostema3/estructuras.datos/EjerciciosEstructurasDatos.py
@@ -6,15 +6,62 @@ Borra al administrador Juan del conjunto de administradores.
 Añade a Marcos como un nuevo administrador, pero no lo borres del conjunto de usuarios.
 Muestra todos los usuarios por pantalla de forma dinámica, además debes indicar cada usuario es administrador o no.'''
 
-usuarios= ("Marta", "David", "Elvira", "Juan", "Marcos")
-administradores = ["Juan", "Marta"]
-print(usuarios, administradores)
-administradores.remove("Juan")
-administradores.append(usuarios.__getitem__(1))
-# print(administradores)
-for nombre in usuarios:
-    if nombre in administradores:
-        print("Admin ",nombre)
-    else:
-        print(nombre)
+# usuarios= ("Marta", "David", "Elvira", "Juan", "Marcos")
+# administradores = ["Juan", "Marta"]
+# print(usuarios, administradores)
+# administradores.remove("Juan")
+# administradores.append(usuarios.__getitem__(1))
+# # print(administradores)
+# for nombre in usuarios:
+#     if nombre in administradores:
+#         print("Admin ",nombre)
+#     else:
+#         print(nombre)
 
+'''Aprende con Alf. Ejercicio 2 
+Escribir un programa que pregunte al usuario su nombre, edad, dirección y teléfono y lo guarde en un diccionario. 
+Después debe mostrar por pantalla el mensaje <nombre> tiene <edad> años, vive en <dirección> y su número de teléfono es <teléfono>.'''
+def ejer2(nombre, edad, telfono, direccion):
+    diccionario = {"nombre" : nombre, "edad" : edad, "telfono" : telfono, "direccion":direccion}
+    return print(diccionario["nombre"]+" tiene " +diccionario["edad"] + " años y vive en "+diccionario["direccion"]
+                 + " y su número de telefno es "+diccionario["telfono"])
+
+# print(ejer2("Carlos","12", "147719256", "Calle Mayor"))
+
+'''Aprende con Alf:Ejercicio 3
+Escribir un programa que guarde en un diccionario los precios de las frutas de la tabla, pregunte al 
+usuario por una fruta, un número de kilos y muestre por pantalla el precio de ese número de kilos de fruta. 
+Si la fruta no está en el diccionario debe mostrar un mensaje informando de ello.'''
+
+def ejer3(fruta, kilos):
+    diccionario = {"Platano" : 1.35, "Manzana" : 0.80, "Pera" : 0.85, "Naranja": 0.70}
+    fruta = fruta.capitalize()
+    kilos = float(kilos)
+    if fruta not in diccionario:
+        salida = "La fruta no esta en el diccionario"
+    else:
+        salida = diccionario[fruta] * kilos
+    return salida
+
+# print(ejer3("Platano", 2))
+# print(ejer3("manzana", 4))
+# print(ejer3("pera", 4))
+# print(ejer3("Naranja", 3.5))
+
+'''Aprende con alf: Ejercicio 5
+Escribir un programa que almacene el diccionario con los créditos de las asignaturas de un curso
+ {'Matemáticas': 6, 'Física': 4, 'Química': 5} y después muestre por pantalla los créditos de cada asignatura en el 
+ formato <asignatura> tiene <créditos> créditos, donde <asignatura> es cada una de las asignaturas del curso, y <créditos> son sus créditos. 
+Al final debe mostrar también el número total de créditos del curso.'''
+
+def ejer5():
+    asignaturas = {'Matemáticas': 6, 'Física': 4, 'Química': 5}
+    creditosTot = 0
+    for asignatura in asignaturas:
+        creditosTot += asignaturas[asignatura]
+        print(asignatura +" tiene: "+str(asignaturas[asignatura]) +" créditos")
+    return ("Creditos totales: "+ str(creditosTot))
+
+print(ejer5())
+
+''' '''
--- ejerciciostema3/estructuras.datos/EjerciciosTuplas.py
+++ ejerciciostema3/estructuras.datos/EjerciciosTuplas.py
@@ -1,3 +1,6 @@
+import math
+
+
 '''Ejercicio 1: Crea una función calcular_estadisticas(numeros) que reciba una lista de
 números y devuelva una tupla con:
 ● El valor mínimo.
@@ -21,4 +24,59 @@ def calcular_estadisticas(lista):
 
 numeros = [1, 0, -4, 6, 21]
 
-print(calcular_estadisticas(numeros))
+# print(calcular_estadisticas(numeros))
+
+'''Ejercicio 2: Crea una función distancia(p1, p2) que reciba dos tuplas representando
+puntos en el plano (x, y) y devuelva la distancia entre ellos usando la fórmula:'''
+
+def distancia(p1, p2):
+    return math.sqrt((p1[0] - p2[0]) ** 2 + (p1[1] - p2[1]) ** 2)
+
+
+punto1 = (4, 7)
+punto2 = (9, -11)
+
+# print(distancia(punto1, punto2))
+
+
+'''Ejercicio 3: Crea una función analizar_texto(texto) que devuelva una tupla con:
+● Número total de caracteres.
+● Número de palabras.
+● Primera palabra del texto.
+'''
+def analizar_texto(texto):
+    texto = texto.lower()
+    totalCaracteres = len(texto)
+    totalPalabras = 0
+    primeraPalabra = ""
+    cadenaSplit = texto.split()
+    for palabra in cadenaSplit:
+        totalPalabras += 1
+        primeraPalabra += palabra
+        break
+    return (totalCaracteres,totalPalabras,primeraPalabra)
+
+resultado = analizar_texto("Lorem ipsum dolor sit amet consectetur adipiscing elit arcu at")
+# print(resultado)
+
+
+'''Ejercicio 5: Crea una función analizar_numeros(numeros) que reciba una lista de
+enteros y devuelva una tupla con:
+● El número de elementos pares.
+● El número de elementos impares.
+● La suma total.'''
+def analizar_numeros(numeros):
+    sumaTotal = 0
+    pares = 0
+    inpares = 0
+    for numero in numeros:
+        sumaTotal += numero
+        if numero % 2 == 0:
+            pares += 1
+        else:
+            inpares += 1
+    return (pares, inpares, inpares)
+res = analizar_numeros(numeros)
+print("Cantidad pares "+str(res[0]))
+print("Cantidad inpares "+str(res[1]))
+print("Suma total "+ str(res[2]))
